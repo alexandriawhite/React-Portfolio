0e829383492b87369e00952279ced50c7affe233
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-03-13 22:29:48 -0500

    Contact me is working

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/validators.js';
+import '../assets/css/contact.css';
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (value.trim() === '') {
+      setErrors((prevState) => ({ ...prevState, [name]: 'This field is required.' }));
+    } else {
+      setErrors((prevState) => ({ ...prevState, [name]: null }));
+    }
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = {};
+    if (formData.name.trim() === '') {
+      errors.name = 'This field is required.';
+    }
+    if (formData.email.trim() === '') {
+      errors.email = 'This field is required.';
+    } else if (!validateEmail(formData.email)) {
+      errors.email = 'Invalid email address.';
+    }
+    if (formData.message.trim() === '') {
+      errors.message = 'This field is required.';
+    }
+    setErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      // Do something with the form data
+      console.log(formData);
+    }
+  };
+
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="contact-container">
+      <h1>Contact Me</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        If you have any questions or would like to get in touch, please fill out
+        the form below or email me directly at alexandriawhitedev@gmail.com.
       </p>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+          {errors.name && <span className="error">{errors.name}</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+            required
+          ></textarea>
+          {errors.message && <span className="error">{errors.message}</span>}
+        </div>
+        <button type="submit" className="btn">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
