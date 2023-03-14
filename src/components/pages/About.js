b3eef5dd2c390adc139dd563e1d4ea1c1e57853c
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-03-13 22:00:06 -0500

    About me section is started

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import acwImage from '../assets/ACW.png';
+import '../assets/css/about.css';
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <h1>About Me</h1>
+      <img src={acwImage} className="avatar" alt="Alexandria White" />
+      <p className="about-text">
+        Welcome to my page. I am Alexandria White, a Full Stack Developer currently enrolled in Vanderbilt University's Coding Boot Camp. 
+        I am working diligently to master the most in-demand web development technologies, including HTML5, CSS3, JavaScript, jQuery, React.js, 
+        Node.js, and more. With a rigorous 24-week curriculum and hands-on collaboration with other professionals on real-world projects, 
+        I am gaining valuable experience in full-stack web development, building complex applications to expand my professional portfolio. 
+        I am passionate about applying my skills to create innovative solutions and continuously enhancing my knowledge and expertise as a developer.
       </p>
     </div>
   );
